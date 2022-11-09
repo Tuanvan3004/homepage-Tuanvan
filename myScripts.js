09f6ea77c24f7c9840ae3701f1b02053d9c07589
Tuanvan3004 <111907851+Tuanvan3004@users.noreply.github.com>
2022-11-09 12:20:09 -0600

    Nov.9

---
+++ myScripts.js
@@ -0,0 +1,27 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+   }
+   
+   function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+   }
+
+   if (localStorage["note"]) {
+    var note = localStorage["note"];
+    document.getElementById("note").value = note;
+   }
+   
+   function autoSave() {
+    var note = document.getElementById("note").value;
+    localStorage.setItem("note", note);
+   }
+   
+   
